20684ce0363eb86b67f54d68565d25ea906f8c3f
germino45 <114361293+germino45@users.noreply.github.com>
2022-12-27 19:22:10 -0600

    add script

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+initialCards[
+  ((cardOne = {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  }),
+  (cardTwo = {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  }),
+  (cardThree = {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  }),
+  (cardFour = {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  }),
+  (cardFive = {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  }),
+  (cardSix = {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  }))
+];
