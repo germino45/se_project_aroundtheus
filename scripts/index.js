f51d6b5d3b8afa356b7d5f9447b1cdd5b56bd21c
germino45 <114361293+germino45@users.noreply.github.com>
2023-01-04 22:01:09 -0600

    edit open and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,19 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   }),
 ];
+
+const closeButton = document.querySelector(".modal__close-button");
+const editButton = document.querySelector(".profile__edit-button");
+const modalContainer = document.querySelector(".modal__container");
+
+function openModal() {
+  modalContainer.classList.add("modal_opened");
+}
+
+function closeModal() {
+  modalContainer.classList.remove("modal_opened");
+}
+
+editButton.addEventListener("click", openModal);
+
+closeButton.addEventListener("click", closeModal);
