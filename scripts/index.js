9852b01c348be5df01c7a7c8e0c751f7d2567e72
germino45 <114361293+germino45@users.noreply.github.com>
2023-01-09 19:00:45 -0600

    feat: add scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,91 @@
 let initialCards = [
   (cardOne = {
     name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    link: "https://live.staticflickr.com/65535/50970386722_550d19f11b_b.jpg",
   }),
   (cardTwo = {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    link: "https://www.worldatlas.com/r/w1200-q80/upload/b4/2c/95/shutterstock-569741011.jpg",
   }),
   (cardThree = {
     name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    link: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/bald-mountain-sunset-gina-herbert.jpg",
   }),
   (cardFour = {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    link: "https://media.istockphoto.com/id/155359110/photo/milky-way-in-the-alps.jpg?s=612x612&w=0&k=20&c=q9pv4VxHGZlm0AJRoQMIUjtnPh2RLXmm_MWxya1nq4Q=",
   }),
   (cardFive = {
     name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    link: "https://montblanctreks.com/media/pages/blog/hiking-the-vanoise-national-park/9d3c53526c-1626794965/vanise-at-sunrise-1920x-q40.png",
   }),
   (cardSix = {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    link: "https://expertvagabond.com/wp-content/uploads/lago-di-braies-italy-guide.jpg",
   }),
 ];
 
+/* Elements */
+
+const profileTitle = document.querySelector(".profile__title");
+
+const profileDescription = document.querySelector(".profile__description");
+
+const titleInput = document.querySelector("#profile-title-input");
+
+const descriptionInput = document.querySelector("#profile-description-input");
+
+const form = document.querySelector(".modal__container-form");
+
 const closeButton = document.querySelector(".modal__close-button");
+
 const editButton = document.querySelector(".profile__edit-button");
+
 const modalContainer = document.querySelector(".modal__container");
 
-function openModal() {
-  modalContainer.classList.add("modal_opened");
-}
+const formSaveButton = document.querySelector(".form__save-button");
+
+const cardsList = document.querySelector(".cards__list");
+
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/* Functions */
 
 function closeModal() {
   modalContainer.classList.remove("modal_opened");
 }
 
-editButton.addEventListener("click", openModal);
+function getCardElement(data) {
+  console.log(data);
+}
+
+initialCards.forEach((data) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__text");
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardTitleEl.textContent = data.name;
+  document.write(cardElement);
+  console.log(cardElement);
+  cardsList.append(cardElement);
+});
+
+/* event listeners */
+
+editButton.addEventListener("click", () => {
+  titleInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+
+  modalContainer.classList.add("modal_opened");
+});
 
 closeButton.addEventListener("click", closeModal);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = titleInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closeModal();
+});
